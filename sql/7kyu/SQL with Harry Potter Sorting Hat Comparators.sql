1cd661b01ec1ae52a252707d989bfe666f1df2ca
xlf-summer <xlf-summer@qq.com>
2019-08-18 19:04:42 +0800

    solve SQL with Harry Potter Sorting Hat Comparators

---
+++ sql/7kyu/SQL with Harry Potter Sorting Hat Comparators.sql
@@ -0,0 +1,7 @@
+/* https://www.codewars.com/kata/sql-with-harry-potter-sorting-hat-comparators/train/sql */
+
+SELECT * FROM students WHERE 
+(quality1 = 'evil' AND quality2 = 'cunning') OR
+(quality1 = 'brave' AND quality2 != 'evil')   OR
+(quality1 = 'studious' OR quality2 = 'intelligent') OR
+(quality1 = 'hufflepuff' OR quality2 = 'hufflepuff');
